bdc5164a063d47791cecbd06ba6c851d8a712317
Tom Crayford <tcrayford@googlemail.com>
2015-02-13 17:29:37 +0800

    globally exclude clojure

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ^:replace ["-server" "-Xmx1g" "-Xms1g" "-XX:+AggressiveOpts" "-XX:+UseFastAccessorMethods"]
+  :global-exclusions [org.clojure/clojure]
   :dependencies [[com.yellerapp/clojure-unrolled-assoc "1.7.0-unrollassoc-SNAPSHOT"]
                  [criterium "0.4.1"]])
